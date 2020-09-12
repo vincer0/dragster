983063eb05f3762933f7c8eec7a6f63501f0bc21
Vincer0 <pasierb.adam@gmail.com>
2020-09-12 12:46:04 +0200

    feat: Add TicketListItem component

---
+++ src/components/TicketListItem/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import styles from './styles';
+
+const TicketListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TicketListItem
---
+++ src/components/TicketListItem/styles.ts
@@ -0,0 +1,4 @@
+import { css, SerializedStyles } from "@emotion/core";
+
+export default (): SerializedStyles => css`
+`;
