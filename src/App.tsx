9f3da2c81cfbf47cc47a33da55f10193f865bf52
Vincer0 <pasierb.adam@gmail.com>
2020-09-12 12:45:16 +0200

    fix: Add default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import './App.css';
+import Drawer from './components/Drawer';
+
 function App() {
   return (
     <div className="App">
+      <Drawer></Drawer>
     </div>
   );
 }
