5b9569d1b813064e1944435d330b4ccdfc6687b7
Vincer0 <pasierb.adam@gmail.com>
2020-09-12 12:45:51 +0200

    feat: Add TicketList component

---
+++ src/components/TicketList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import styles from './styles';
+
+const TicketList = (): JSX.Element => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TicketList
