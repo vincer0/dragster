c200d65a7f69583daa56b9c0373c144c6a0008d4
Vincer0 <pasierb.adam@gmail.com>
2020-09-12 12:44:53 +0200

    feat: Add Drawer component

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import styles from './styles';
+
+const Drawer = () => {
+    return (
+        <div css={styles}>
+            
+        </div>
+    )
+}
+
+export default Drawer
+
---
+++ src/components/Drawer/styles.ts
@@ -0,0 +1,9 @@
+import { css, SerializedStyles } from "@emotion/core";
+
+export default (): SerializedStyles => css`
+    position: fixed;
+    height: 100%;
+    background-color: #6F767A;
+    width: 242px;
+    left: 0;
+`;
