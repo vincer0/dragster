8a379fe4e6599ede87502524202953d8320edd56
Vincer0 <pasierb.adam@gmail.com>
2020-09-13 20:02:45 +0200

    fix: Added box-sizing and default padding value

--- src/components/Drawer/styles.ts
+++ src/components/Drawer/styles.ts
@@ -1,9 +1,11 @@
 import { css, SerializedStyles } from "@emotion/core";
 
 export default (): SerializedStyles => css`
+    box-sizing: border-box;
     position: fixed;
     height: 100%;
     background-color: #6F767A;
     width: 242px;
     left: 0;
+    padding: 16px;
 `;
